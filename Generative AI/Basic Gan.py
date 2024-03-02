a8a1d0b06480d86447c564d1edefc84aa871d190
Mandoo <wjddms6841@gmail.com>
2024-03-02 10:54:16 +0900

    finish basic gan

--- Generative AI/Basic Gan.py
+++ Generative AI/Basic Gan.py
@@ -128,3 +128,39 @@ def calc_disc_loss(loss_func, gen, disc, number, real_img, z_dim):
     disc_loss = (disc_fake_loss + disc_real_loss) / 2  # average loss 
 
     return disc_loss
+# %% 60000 / 128 = around 469 steps in each epoch
+### Each step is going to process 128 images = BATCH SIZE (except the last step)
+
+for epoch in range(epochs):
+    for real, _ in tqdm(dataloader):    # _ = labels that aren't needed since it's not supervised learning
+        ### discriminator
+        disc_opt.zero_grad()
+
+        cur_bs = len(real)  # real: 128 x 1 x 28 x 28 (1 refers to the number of channels, thus grayscale here)
+        real = real.view(cur_bs, -1)    # 128 x 784
+        real = real.to(device)
+        
+        disc_loss = calc_disc_loss(loss_func, gen, disc, cur_bs, real, z_dim)
+
+        disc_loss.backward(retain_graph=True)
+        disc_opt.step()
+
+        ### generator 
+        gen_opt.zero_grad()
+        gen_loss = calc_gen_loss(loss_func, gen, disc, cur_bs, z_dim)
+        gen_loss.backward(retain_graph=True)
+        gen_opt.step()
+
+        ### visualization & stats
+        mean_disc_loss += disc_loss.item() / info_step 
+        mean_gen_loss += disc_loss.item() / info_step
+
+        if cur_step % info_step == 0 and cur_step > 0 :
+            fake_noise = gen_noise(cur_bs, z_dim)
+            fake = gen(fake_noise)
+            show(fake)
+            show(real)
+            print(f"{epoch}: step {cur_step} / Gen Loss: {mean_gen_loss} / disc_loss: {mean_disc_loss}")
+            mean_gen_loss, mean_disc_loss = 0, 0    # init for the new step
+        cur_step += 1      
+# %%
