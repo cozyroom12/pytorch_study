3a2bce943411c824c0ea5a500088dcca63913865
Mandoo <wjddms6841@gmail.com>
2024-03-05 23:29:27 +0900

    Add generator class WGAN

--- Generative AI/Advanced Gan (WGAN).py
+++ Generative AI/Advanced Gan (WGAN).py
@@ -10,4 +10,27 @@ import numpy as np
 from PIL import Image 
 import matplotlib.pyplot as plt 
 
-def show(tensor, num=25, wamb=0, name):
+def show(tensor, num=25, name=''):
+    data = tensor.detach().cpu()
+    grid = make_grid(data[:num], nrow=5).permute(1,2,0) # plt
+
+    plt.imshow(grid.clip(0,1))
+    plt.show()
+
+### hyperparameters and general parameters
+n_epochs = 10000
+batch_size = 128
+lr = 1e-4
+z_dim = 200
+device = 'cuda'
+
+cur_step = 0
+crit_cycles = 5
+gen_losses = []
+crit_losses = []
+show_step = 35 
+save_step = 35  # checkpoint save
+# %% generator model
+
+class Generator(nn.Module):
+    def __init__(self, z_dim=64, d_dim)
