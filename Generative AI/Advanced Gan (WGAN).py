e9d335a71bfa5881af1266d7e4fde511696591fd
Mandoo <wjddms6841@gmail.com>
2024-03-05 23:16:56 +0900

    first start WGAN

--- Generative AI/Advanced Gan (WGAN).py
+++ Generative AI/Advanced Gan (WGAN).py
@@ -1,2 +1,13 @@
 #%% 
-## Wasserstein Loss
+import torch, torchvision, os, PIL, pdb 
+from torch import nn 
+from torch.utils.data import Dataset
+from torch.utils.data import DataLoader 
+from torchvision import transforms 
+from torchvision.utils import make_grid 
+from tqdm.auto import tqdm 
+import numpy as np 
+from PIL import Image 
+import matplotlib.pyplot as plt 
+
+def show(tensor, num=25, wamb=0, name):
